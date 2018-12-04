1e48ec9a9bb6d34366621ef420487ba651d195eb
Lin Hsu <froggy2818@gmail.com>
2018-12-04 14:54:30 +0800

    feat: wire up the login form

--- src/middleware.js
+++ src/middleware.js
@@ -5,6 +5,7 @@ function isPromise(v) {
 
 const promiseMiddleware = store => next => action => {
     if (isPromise(action.payload)) {
+        store.dispatch({ type: 'ASYNC_START', subtype: action.type })
         action.payload.then(
             res => {
                 action.payload = res;
